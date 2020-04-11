a46d759e4a77cd143ae32c1d81d7c200bda961ed
Will Jones <willjones484@gmail.com>
2020-04-11 21:03:27 +1000

    deleted compiled js

--- odd-positive-integer-average/src/util.spec.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const util_1 = require("./util");
-// Test suite here...
-test('averageOddPositiveIntegers function exists', () => {
-    expect(util_1.averageOddPositiveIntegers).toBeDefined();
-});
