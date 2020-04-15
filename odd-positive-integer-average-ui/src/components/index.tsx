144499cc6d4ed2019a7ad102edff976a51206d03
Will Jones <willjones484@gmail.com>
2020-04-15 17:13:48 +1000

    added gui using form data

--- odd-positive-integer-average-ui/src/components/index.tsx
+++ odd-positive-integer-average-ui/src/components/index.tsx
@@ -1,39 +1,37 @@
-import React, { Component } from "react";
-import { clientRequest } from "../util/index";
-// The main application file.
+import React from "react";
+import clientRequest from "../util/index";
 
-const onSubmit = (e) => {
-  e.preventDefault();
-  console.log("submitting");
+interface Props {}
+interface Data {
+  userData: string;
+}
 
-  clientRequest();
-};
+export default class Counter extends React.Component<Props, Data> {
+  state: Data = {
+    userData: "",
+  };
 
-const App = () => {
-  return (
-    <div className="app">
-      <div className="app__header">
-        <h3 className="app__header__title">Odd Positive Average Calculator!</h3>
+  onChange = (e: any) => {
+    this.setState({ userData: e.target.value });
+  };
+
+  onClick = (e: any) => {
+    clientRequest(this.state.userData);
+  };
+
+  render() {
+    return (
+      <div>
+        <h3>Input your data separated by comma (',')</h3>
+        <input
+          type="text"
+          id="userData"
+          onChange={this.onChange}
+          value={this.state.userData}
+          placeholder="Input Data"
+        />
+        <button onClick={this.onClick}>Submit Data</button>
       </div>
-      <div className="app__container">
-        <div className="app__container__form">
-          <form onSubmit={onSubmit}>
-            <div className="form__group">
-              <input
-                type="text"
-                name="data"
-                id="user-data"
-                placeholder="Array Data"
-              />
-            </div>
-            <button type="submit" onClick={clientRequest}>
-              Find Average
-            </button>
-          </form>
-        </div>
-      </div>
-    </div>
-  );
-};
-
-export default App;
+    );
+  }
+}
--- odd-positive-integer-average-ui/src/util/index.ts
+++ odd-positive-integer-average-ui/src/util/index.ts
@@ -2,18 +2,24 @@
 
 var axios = require("axios");
 
-export const clientRequest = () => {
-  console.log("request sent");
+export const clientRequest = (userData) => {
+  console.log("sending request");
+
+  const sendData: number[] = userData.split(",").map(Number);
+
+  console.log(sendData);
 
   axios({
     method: "POST",
     url: "/odd-positive-average",
-    data: [1],
+    data: sendData,
   })
     .then((res: any) => {
-      console.log("Average of all positive integers:");
+      console.log(`Average of all positive integers: ${res.data.average}`);
     })
     .catch((err: any) => {
       console.log("No odd positive integers were supplied.");
     });
 };
+
+export default clientRequest;
