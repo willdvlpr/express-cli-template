04828da73b60190c4c1ea367d18d60b9ef938bae
Will Jones <willjones484@gmail.com>
2020-04-16 22:54:38 +1000

    chnage from class to functional comp.

--- odd-positive-integer-average-ui/src/components/index.tsx
+++ odd-positive-integer-average-ui/src/components/index.tsx
@@ -1,64 +1,21 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import clientRequest from "../util/index";
 import axios from "axios";
 
-interface Props {}
-interface Data {
-  userData: string;
-  userAverage: number;
-}
+const App: React.FC = () => {
+  const [userData, setData] = useState("");
+  const [userAverage, setAverage] = useState(0);
+  return (
+    <div>
+      <h3>Input your data separated by comma (',')</h3>
+      <input
+        type="text"
+        value={userData}
+        onChange={(e) => setData(e.target.value)}
+      />
+      <button onClick={(e) => clientRequest(userData)}>Submit Data</button>
+    </div>
+  );
+};
 
-export default class Counter extends Component<Props, Data> {
-  state: Data = {
-    userData: "",
-    userAverage: 0,
-  };
-
-  onChange = (e: any) => {
-    this.setState({ userData: e.target.value });
-  };
-
-  onClick = (e: any) => {
-    clientRequest(this.state.userData);
-  };
-
-  onHandle = (e: any) => {
-    this.getData();
-  };
-
-  getData = () => {
-    axios
-      .get("/odd-positive-average")
-      .then((response: any) => {
-        this.setData(Number(response.data));
-      })
-      .catch((err: any) => {
-        console.log(err);
-      });
-  };
-
-  setData = (average: number) => {
-    this.setState({ userAverage: average });
-    console.log(this.state.userAverage);
-  };
-
-  render() {
-    return (
-      <div>
-        <h3>Input your data separated by comma (',')</h3>
-        <input
-          type="text"
-          id="userData"
-          onChange={this.onChange}
-          value={this.state.userData}
-          placeholder="Input Data"
-        />
-        <button onClick={this.onClick}>Submit Data</button>
-        <button onClick={this.onHandle}>Get Average of Data</button>
-        <div className="display__data">
-          <h3>Average is {this.state.userAverage}</h3>
-        </div>
-      </div>
-    );
-  }
-}
+export default App;
